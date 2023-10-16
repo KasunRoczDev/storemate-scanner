569c1e450942e5797b26a39798cea05c12173e15
KasunRoczDev <108571427+KasunRoczDev@users.noreply.github.com>
2023-10-16 06:57:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import "./styles.css";
 import { Html5Qrcode } from "html5-qrcode";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 
 let html5QrCode;
 export default function App() {
+    const [detectionCount, setDetectionCount] = useState(0);
+
     useEffect(() => {
         handleClickAdvanced();
     }, []);
 
+
     const qrConfig = {fps: 10, qrbox: { width: 200, height: 200 }
+    };
+
+    const resetDetectionCount = () => {
+        setDetectionCount(0);
     };
 
     //Start Scanning and capturing data
     const handleClickAdvanced = () => {
         const html5QrCode = new Html5Qrcode("reader");
         const qrCodeSuccessCallback = (decodeText, decodedResult) => {
-            window.parent.postMessage(
-                {
-                    event_id: 'add_scanned_product',
-                    data: {
-                        sku: decodeText
-                    }
-                },
-                "*"
-            );
-            handleStop();
+            if (detectionCount === 0) {
+                setDetectionCount(1);
+                window.parent.postMessage(
+                    {
+                        event_id: 'add_scanned_product',
+                        data: {
+                            sku: decodeText
+                        }
+                    },
+                    "*"
+                );
+
+                handleStop();
+                setTimeout(resetDetectionCount, 3600);
+            }
         };
 
         html5QrCode
